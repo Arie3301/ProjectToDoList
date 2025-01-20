cd614ef20b24c7d60e2c9eb08fbe5578f927f91e
Arie3301 <arie_slobbe@hotmail.com>
2025-01-20 12:34:56 +0100

    Init Spring Boot backend step 2: non-git

---
+++ Backend/ProjectToDoList/src/main/java/asl/ProjectToDoList/ProjectToDoListApplication.java
@@ -0,0 +1,13 @@
+package asl.ProjectToDoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectToDoListApplication.class, args);
+	}
+
+}
---
+++ Backend/ProjectToDoList/src/test/java/asl/ProjectToDoList/ProjectToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package asl.ProjectToDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
